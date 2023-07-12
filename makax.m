d0f4f3c53092f4d047404faf93b9bc2d984d6956
Jackson Smith <22416446+jsdpag@users.noreply.github.com>
2023-07-12 16:31:56 +0200

    No longer prints input axes parameters.

--- makax.m
+++ makax.m
@@ -87,7 +87,7 @@ if  nargin  &&  isgraphics( varargin{ 1 } , 'axes' )
   ax = varargin{ 1 } ;
   
   % Apply user input arguments
-  set( ax , varargin{ 2 : end } )
+  if  nargin > 1 , set( ax , varargin{ 2 : end } ) ; end
   
 % Axes not provided
 else
@@ -121,13 +121,13 @@ end % input args
 %%% Apply default parameters %%%
 
 % Apply default axes parameters
-set( ax , DEFPAR{ : } )
+set( ax , DEFPAR{ : } ) ;
 
 % Axis labels
 for  I = { 'Title' , 'XLabel' , 'YLabel' , 'ZLabel' } , i = I{ 1 } ;
   
   % Apply default label parameters
-  set( ax.( i ) , AXIPAR{ : } )
+  set( ax.( i ) , AXIPAR{ : } ) ;
   
 end % labels
 
